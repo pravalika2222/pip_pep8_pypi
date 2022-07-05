007a280bbdd235c0c5a235a038470e94eb723810
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-05 22:00:55 +0530

    Add files via upload

---
+++ pip_pep8_pypi.py
@@ -0,0 +1,12 @@
+'''pip __> python installation package'''
+'pip  is the package manager for python'
+
+"pep8 __>python enhanched proposal"
+'pep8 is used for coding style convention'
+
+"pypi --> python package index"
+
+'pypi.org --> website for all package available in python'
+
+
+
